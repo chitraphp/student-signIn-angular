1919b4daec6f7a881f0494656f0f2599b49dc666
chitra <chitra.atmakuru@gmail.com>
2015-04-24 15:49:13 -0700

    Add changes

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,5 +1,8 @@
-studentsSignIn.controller('StudentsCtrl', function StudentsCtrl($scope) {
+studentsSignIn.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
 
-  $scope.students = [{name:"chitra",presence:false},{name:"kenna",presence:false},{name:"cynthia",presence:false},{name:"Nhu",presence:false}, {name:"Erica cha",presence:false},{name:"Boyana",presence:false},{name:"Liz",presence:false},{name:"suchi",presence:false}];
-
+  $scope.students = StudentsFactory.students;
+  $scope.StudentsFactory = StudentsFactory;
+  // $scope.makePresence = function (student) {
+  //     student.presence = true;
+  // }
 });
