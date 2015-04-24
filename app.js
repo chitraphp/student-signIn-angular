37d63ba981ac4222a991749df88a74407ac25a8a
chitra <chitra.atmakuru@gmail.com>
2015-04-24 13:56:15 -0700

    Add presence.html

---
+++ app.js
@@ -0,0 +1,21 @@
+var studentsSignIn = angular.module('studentsSignIn', ['ui.router']);
+
+studentsSignIn.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html"
+  });
+
+  $stateProvider.state('signIn', {
+    url: "/signIn",
+    templateUrl: "partials/sign-in.html",
+    controller: 'StudentsCtrl'
+  });
+
+  $stateProvider.state('presence', {
+    url: "/presence",
+    templateUrl: "partials/presence.html",
+    controller: 'StudentsCtrl'
+  });
+
+});
---
+++ controllers/StudentsController.js
@@ -0,0 +1,5 @@
+studentsSignIn.controller('StudentsCtrl', function StudentsCtrl($scope) {
+
+  $scope.students = [{name:"chitra",presence:false},{name:"kenna",presence:false},{name:"cynthia",presence:false},{name:"Nhu",presence:false}, {name:"Erica cha",presence:false},{name:"Boyana",presence:false},{name:"Liz",presence:false},{name:"suchi",presence:false}];
+
+});
